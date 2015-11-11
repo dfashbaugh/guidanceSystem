9c84d43d0d479f0a68897424a03f2dbb34009d9a
bbqdave <dsheinkopf@Davids-iMac.local>
2015-11-10 21:26:38 -0500

    boat direction
    first update

---
+++ BoatOrientationSensor/BoatOrientationSensor.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+
+/* 
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+*/
+
+
+#define BNO055_SAMPLERATE_DELAY_MS (50)
+   
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+void setup(void) 
+{
+  Serial.begin(9600);  
+  Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
+  
+  if(!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+  bno.setExtCrystalUse(true);
+}
+
+
+void loop(void) 
+{
+  // Possible vector values can be:
+  // - VECTOR_ACCELEROMETER - m/s^2
+  // - VECTOR_MAGNETOMETER  - uT
+  // - VECTOR_GYROSCOPE     - rad/s
+  // - VECTOR_EULER         - degrees
+  // - VECTOR_LINEARACCEL   - m/s^2
+  // - VECTOR_GRAVITY       - m/s^2
+
+  
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    imu::Vector<3> magnet = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+
+
+
+// absolute position in degrees
+float heading = atan2(magnet.x(), magnet.y());
+Serial.println(heading* (57296 / 1000));
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
